572647f32144b58f5b9154b73b4bb5e164e0a013
mathew <chavrymsthew@gmail.com>
2023-12-01 14:05:12 -0500

    mAs

---
+++ SIngle view app/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  SIngle view app
+//
+//  Created by 8h on 12/1/23.
+//
+
+import Foundation
+ 
+struct Quiz{
+    var questions: [[String: Any]] = [
+        
+        }
+    
+    ]
+}
